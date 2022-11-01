b13b05bb07dfb602fcdc1ca58d04af2fdd9996c7
Rifat Jayed <rifat.jayed2000@gmail.com>
2022-11-01 22:07:26 +0600

    address update

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 document.querySelector("#btn3").addEventListener("click",function(e){
 e.preventDefault();
   document.getElementById("popup").style.display = "block";
+  document.querySelector(".herosec").style.opacity ="0.5";
 })
 document.querySelector(".close-btn").addEventListener("click",function(){
     document.getElementById("popup").style.display = "none";
+    document.querySelector(".herosec").style.opacity ="1";
 })
